2e7585c04f8b72b167d458a35b1004b10678d455
GamBar <vlpetrov02@gmail.com>
2024-07-04 13:34:20 +0300

    Added more animations, removed a console log

--- src/sections/SkillSection.svelte
+++ src/sections/SkillSection.svelte
@@ -5,9 +5,15 @@
   import Skills from '@components/Skills.svelte';
   import inView from '@utils/inView';
   import i18n from '@utils/i18n';
+  import { fly } from 'svelte/transition';
 
   export let experience;
   export let numberOfProjects;
+
+  let animateJS = false;
+  let animateCSharp = false;
+  let animateTools = false;
+  let animateSkills = false;
 </script>
 
 <div id="skills" use:inView={{ threshold: 0.2 }} on:enter={() => ($location = 'skills')}>
@@ -16,29 +22,57 @@
       <Stats {numberOfProjects} />
     </div>
   </div>
-  <div class="bg-yellow-300 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2">
-    <h1 use:i18n class="text-3xl font-semibold">JavaScript</h1>
-    <div use:i18n class="max-w-3xl mx-auto my-4">
-      JavaScript is my language of choice for doing almost anything, but I am looking to expand my arsenal.
+  {#if animateJS}
+    <div
+      in:fly={{ duration: 500, x: -2000, opacity: 1 }}
+      class="bg-yellow-300 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2"
+    >
+      <h1 use:i18n class="text-3xl font-semibold">JavaScript</h1>
+      <div use:i18n class="max-w-3xl mx-auto my-4">
+        JavaScript is my language of choice for doing almost anything, but I am looking to expand my arsenal.
+      </div>
+      <Skills skillSet={experience.js} />
     </div>
-    <Skills skillSet={experience.js} />
-  </div>
-  <div class="bg-purple-500 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2">
-    <h1 use:i18n class="text-3xl font-semibold">C Sharp</h1>
-    <div use:i18n class="max-w-3xl mx-auto my-4">
-      Started learning C# in SoftUni on 13-05-2024. Stay tuned for updates!
+  {:else}
+    <div class="mt-60 p-2" use:inView on:enter={() => (animateJS = true)}></div>
+  {/if}
+  {#if animateCSharp}
+    <div
+      in:fly={{ duration: 500, x: 2000, opacity: 1 }}
+      class="bg-purple-500 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2"
+    >
+      <h1 use:i18n class="text-3xl font-semibold">C Sharp</h1>
+      <div use:i18n class="max-w-3xl mx-auto my-4">
+        Started learning C# in SoftUni on 13-05-2024. Stay tuned for updates!
+      </div>
     </div>
-  </div>
-  <div class="bg-blue-500 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2">
-    <h1 use:i18n class="text-3xl font-semibold">Tools</h1>
-    <Skills skillSet={experience.other} />
-  </div>
-  <div class="bg-neutral-200 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2">
-    <h1 use:i18n class="text-3xl font-semibold">Soft Skills</h1>
-    <div use:i18n class="max-w-3xl mx-auto my-4">
-      Given my experience in working with others on large projects, I have gained valuable skills for working in a team
-      with other people.
+  {:else}
+    <div class="mt-44 p-2" use:inView on:enter={() => (animateCSharp = true)}></div>
+  {/if}
+  {#if animateTools}
+    <div
+      in:fly={{ duration: 500, x: -2000, opacity: 1 }}
+      class="bg-blue-500 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2"
+    >
+      <h1 use:i18n class="text-3xl font-semibold">Tools</h1>
+      <Skills skillSet={experience.other} />
     </div>
-    <Skills skillSet={experience.softSkills} />
-  </div>
+  {:else}
+    <div class="mt-60 p-2" use:inView on:enter={() => (animateTools = true)}></div>
+  {/if}
+  {#if animateSkills}
+    <div
+      in:fly={{ duration: 500, x: 2000, opacity: 1 }}
+      class="bg-neutral-200 text-black slanted-2 text-center py-12 px-4 my-4 md:my-0 xl:-my-2"
+    >
+      <h1 use:i18n class="text-3xl font-semibold">Soft Skills</h1>
+      <div use:i18n class="max-w-3xl mx-auto my-4">
+        Given my experience in working with others on large projects, I have gained valuable skills for working in a
+        team with other people.
+      </div>
+      <Skills skillSet={experience.softSkills} />
+    </div>
+  {:else}
+    <div class="mt-60 p-2" use:inView on:enter={() => (animateSkills = true)}></div>
+  {/if}
 </div>
